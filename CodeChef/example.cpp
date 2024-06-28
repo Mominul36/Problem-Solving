47a91de1d0f1cfe370b5e8cbf1864c69f060e078
Mominul36 <mominulislam23295@gmail.com>
2024-06-29 00:46:17 +0600

    Random

---
+++ CodeChef/example.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+
+#define GET_FASTER ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define watch(c) for(auto i: c){cout<< i <<" ";}cout<<endl;
+#define watch2(c) for(auto i: c){for(auto j : i){cout<< j <<" ";}cout<<endl;}
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define getI(a) scanf("%d", &a)
+#define getII(a,b) scanf("%d%d", &a, &b)
+#define getIII(a,b,c) scanf("%d%d%d", &a, &b, &c)
+void input(vector<int>&v, int n) {for(int i=0; i<n; i++){int x; cin>>x; v.push_back(x);}}
+
+
+int main(){
+    GET_FASTER;
+     int a;
+     cin>>a;
+     cout<<a<<endl;
+
+
+
+    
+   return 0; 
+}
